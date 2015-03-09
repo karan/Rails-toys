a692b5305ab95082cdd1108b408adfb475ceed52
Karan Goel <karan@goel.io>
2015-03-09 16:26:45 -0700

    make basic user model with secure pwd

---
+++ 2-sample_app/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  validates :name, presense: true, length: { maximum: 50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presense: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimun: 6 }
+end
