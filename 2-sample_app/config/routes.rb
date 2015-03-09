b1b54df70d73a317b430446402a96916cd509cce
Karan Goel <karan@goel.io>
2015-03-09 16:05:58 -0700

    add contact and about pages

--- 2-sample_app/config/routes.rb
+++ 2-sample_app/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   get '/help' => 'static_pages#help' 
+  get '/contact' => 'static_pages#contact'
+  get '/about' => 'static_pages#about'
 
   root 'static_pages#home'
 end
