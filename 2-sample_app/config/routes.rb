f8eac706b4444fe039bcdd8b419712e49f416dcb
Karan Goel <karan@goel.io>
2015-03-09 16:08:11 -0700

    clean up routes a bit

--- 2-sample_app/config/routes.rb
+++ 2-sample_app/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
-  get '/help' => 'static_pages#help' 
-  get '/contact' => 'static_pages#contact'
-  get '/about' => 'static_pages#about'
-
   root 'static_pages#home'
+
+  get 'help' => 'static_pages#help' 
+  get 'contact' => 'static_pages#contact'
+  get 'about' => 'static_pages#about'
+
 end
