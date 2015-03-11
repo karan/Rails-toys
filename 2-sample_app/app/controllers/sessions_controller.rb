571886d19201f99c08944a60ee3bd93912dfd01d
Karan Goel <karan@goel.io>
2015-03-10 18:16:03 -0700

    start adding sessions support

--- 2-sample_app/app/controllers/sessions_controller.rb
+++ 2-sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # login
+      log_in user
+      redirect_to user
+    else
+      # create error
+      flash[:danger] = 'Invalid email/password'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
